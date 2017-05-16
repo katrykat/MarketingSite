9e51cc1e0f9f6c59c490a67cf4d24dd8cdbe68e7
Kateris Martinez <katerismartinez@gmail.com>
2017-05-16 13:39:42 +0100

    nav

--- js/burguer.js
+++ js/burguer.js
@@ -13,7 +13,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 5000, 'easeInOutExpo');
         event.preventDefault();
     });
 });
